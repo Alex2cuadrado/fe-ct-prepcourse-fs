90a275d9db286ccd52252cb660365e4616d306fa
josedanielap <jdap.1988@gmail.com>
2023-11-21 22:14:51 -0300

    homework 4 y 5

---
+++ M04 JavaScript Bucles/exam.js
@@ -0,0 +1,166 @@
+/*
+ * Completa la función 'sumaTodosPrimos' a continuación.
+ *
+ * La función debe retornar un ENTERO.
+ * La función recibe un ARREGLO DE ENTEROS como parámetro.
+ *
+ * No modifiques nada por fuera del cuerpo de la función.
+ */
+
+function sumaTodosPrimos(numeros) {
+    // tu código aquí
+var num = numeros
+   if (num <= 1){
+       return false
+   }
+   
+   for (var i = 2; i < num; i++){
+   if(num % i == 0)
+    return false   
+    }
+    return true
+}
+console.log(sumaTodosPrimos())
+
+/*
+ * Completa la función 'buscaMinMax' a continuación.
+ *
+ * La función deberia retornar un STRING.
+ * La función recibe como parámetro un ARREGLO DE NÚMEROS.
+ *
+ * No modifiques nada por fuera del cuerpo de la función.
+ */
+//
+/*5
+1
+12
+5
+22
+8
+Your Output (stdout)
+~ no response on stdout ~
+Expected Output
+El número menor es 1 y el mayor es 22*/
+function buscaMinMax(arrNumeros) {
+    // Tu código aquí
+    var num = arrNumeros
+    var num1 = num >=  0
+    var num2 = num1 < num.length
+    for(var i = 0;i < num.length; i++){
+        if(num1 = num > 0 ){   
+          return num1
+        }
+    else if( num2 = num1 < num.length())
+    return num2
+    }
+}
+console.log(buscaMinMax("el numero" + num1 + "es menor"+ " y" + "el numero" +num2 + "es mayor"))
+
+Completa la clase 'Empleado' a continuación.
+ *
+ * No modifiques nada por fuera del cuerpo de los métodos.
+ */
+
+class Empleado {
+    constructor(nombre, apellido, salario) {
+        // tu código aquí
+    }
+
+    getNombreCompleto() {
+        // tu código aquí
+        nombre : 
+    }
+
+    getSalario() {
+        // tu código aquí
+        salario : 1000
+    }
+
+    setSalario(nuevoSalario) {
+        // tu código aquí
+        nuevoSalario: 1200
+    }
+
+}
+console.log()
+
+function main() {}
+
+/*
+ * Completa la función 'bienvenidoSr' a continuación.
+ *
+ * La función debe retornar un STRING.
+ * La función recibe como parámetro un OBJETO 'persona'.
+ *
+ * No modifiques nada por fuera del cuerpo de la función.
+ */
+
+function bienvenidoSr(persona) {
+    //Tu código aquí
+}
+
+function main() {}
+
+/*
+ * Completa la función 'ingredienteEnMalEstado' a continuación.
+ *
+ * La función debe retornar un ARRAY DE STRING ó un STRING.
+ * La función recibe los siguientes parámetro(s):
+ *  1. OBJETO 'menu'
+ *  2. STRING 'comida'
+ *  3. STRING 'ingrediente'
+ *
+ * No modifiques nada por fuera del cuerpo de la función.
+ * 
+ */
+
+function ingredienteEnMalEstado(menu, comida, ingrediente) {
+    //Tu código aquí
+}
+
+function main() {}
+
+/*
+ * Completa la función 'envolverRegalo'.
+ *
+ * Recibe un ARREGLO DE STRINGS.
+ * Retorna un ARREGLO DE STRINGS.
+ */
+
+function envolverRegalo(regalos) {
+    // Tu código aquí
+}
+
+function main() {}
+
+
+/*
+ * Completa la función 'personajesTargaryen' a continuación.
+ *
+ * La función debe retornar un STRING_ARRAY.
+ * La función recibe un STRING_ARRAY como parámetro.
+ *
+ * No cambies nada por fuera del cuerpo de la función.
+ */
+
+function personajesTargaryen(personajes) {
+    // tu código aquí
+}
+
+function main() {}
+
+
+/*
+ * Completa la función 'mismaCantidadCaracteres' a continuación.
+ *
+ * La función debe retornar un ARREGLO DE STRINGS.
+ * La función recibe un ARREGLO DE STRINGS 'strings' y un ENTERO 'caracteres' como parámetros.
+ *
+ * No modifiques nada por fuera del cuerpo de la función.
+ */
+
+function mismaCantidadCaracteres(strings, caracteres) {
+    // Tu código aquí
+}
+
+function main() {
